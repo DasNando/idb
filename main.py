443fee4c9a0bf0fa3b3ca03c5b7d94134034283c
Fernando Mendoza <fernandom@utexas.edu>
2017-03-21 21:21:41 -0500

    testing upload to Google Cloud/SWE-readers.me

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import logging
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -18,5 +21,31 @@ def profile(username):
 def show_post(post_id):
     return 'post id is: %d' % post_id
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+
 if __name__ == '__main__':
     app.run()
+
