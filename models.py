03d2b8c8e94c86721aa51255b5b6245fb2d9714e
Conner Newman <conner@cs.utexas.edu>
2017-03-23 16:50:56 -0500

    added more comments and fixed that one problem

--- models.py
+++ models.py
@@ -1,140 +1,157 @@
 from main import db
 
+# pylint: disable = bad-whitespace
+# pylint: disable = invalid-name
+# pylint: disable = missing-docstring
+# pylint: disable = too-few-public-methods
+# pylint: disable = too-many-arguments
+# pylint: disable = too-many-instance-attributes
+# pylint: disable = pointless-string-statement
+# pylint: disable = missing-docstring
+
 """
 Models:
 
 Book
-
-Author:
-
-Publisher:
-
-Reviews:
+Author
+Publisher
+Reviews
 """
 
 
 class Book(db.Model):
-    """Links to Author, Review, Publisher"""
+    """Links to Author, Review, Publisher
+       Book-Author and Book-Publisher are one-to-one relationships,
+       Book-Review is one-to-many"""
     title = db.Column(db.String(120), primary_key=True)
-    publisher = db.relationship('Publisher', uselist=False, backref='book', lazy='dynamic')  # db.Column(db.String(120))
-    genre = db.Column(db.String(120))  # This is multiple, so let's figure that out
-    author = db.relationship('Author', uselist=False, backref='book', lazy='dynamic')  # db.Column(db.String(80))
+    publisher = db.relationship('Publisher', uselist=False, backref='book', lazy='dynamic')
+    genre = db.Column(db.String(120))
+    author = db.relationship('Author', uselist=False, backref='book', lazy='dynamic')
     year = db.Column(db.Integer)
     edition = db.Column(db.Integer)
-    ISBN = db.Column(db.String(80))
+    isbn = db.Column(db.String(80))
     prices = db.Column(db.Float)
     reviews = db.relationship('Review', backref='book', lazy='dynamic')
 
-     # cover_art = db.Column() ???
+    def __init__(self, title, publisher, genre, author, year, edition, isbn, prices, reviews):
+        """All string data members are asserted to be of len > 0, price is asserted to be > 0"""
 
-    def __init__(self, title, publisher, genre, author, year, edition, ISBN, prices, reviews):
         self.title = title
-        assert (len(title) > 0)
+        assert len(title) > 0
 
         self.publisher = publisher
 
         self.genre = genre
-        assert (len(genre) > 0)
+        assert len(genre) > 0
 
         self.author = author
 
         self.year = year
-        assert (0 < year < 2018)
+        assert 0 < year < 2018
 
         self.edition = edition
-        assert (edition > 0)
+        assert edition > 0
 
-        self.ISBN = ISBN
-        assert (len(ISBN) > 0)
+        self.isbn = isbn
+        assert len(isbn) > 0
 
         self.prices = prices
-        assert (prices > 0)
+        assert prices > 0
 
         self.reviews = reviews
 
 
 class Author(db.Model):
-    """Links to Book, Publisher"""
-    alive = db.Column(db.Boolean)  # ???
+    """Links to Book, Publisher
+       Author-Book is a one-to-many relationship,
+       Author-Publisher is one-to-one"""
+    alive = db.Column(db.Boolean)
     name = db.Column(db.String(80), primary_key=True)
     birth_date = db.Column(db.String(80))  # subject to change
     death_date = db.Column(db.String(80))  # ^^^
     works = db.relationship('Book', backref='author',
-                            lazy='dynamic')  # db.Column(db.String(80)) # placeholder, NOT FINAL
+                            lazy='dynamic')
     genre = db.Column(db.String(80))
     publisher = db.relationship('Publisher', uselist=False, backref='author', lazy='dynamic')
 
     def __init__(self, alive, name, birth_date, death_date, works, genre, publisher):
+        """All string members are asserted to be of len > 0"""
+
         self.alive = alive
 
         self.name = name
-        assert (len(name) > 0)
+        assert len(name) > 0
 
         self.birth_date = birth_date
-        assert (len(birth_date) > 0)
+        assert len(birth_date) > 0
 
         self.death_date = death_date
-        assert (len(death_date) > 0)
+        assert len(death_date) > 0
 
         self.works = works
 
         self.genre = genre
-        assert (len(genre) > 0)
+        assert len(genre) > 0
 
         self.publisher = publisher
 
 
 class Publisher(db.Model):
-    """Links to Author, Book"""
+    """Links to Author, Book
+       Publisher-Book and Publisher-Author are one-to-many relationships"""
     name = db.Column(db.String(80), primary_key=True)
-    founding_date = db.Column(db.String(80))  # subject to change
+    founding_date = db.Column(db.String(80))
     headquarters = db.Column(db.String(160))
     country = db.Column(db.String(120))
-    founders = db.Column(db.Column(db.String(160)))  # placeholder, NOT FINAL
-    books = db.relationship('Book', backref='publisher', lazy='dynamic')  # db.Column(db.String(80))   # ^^^
-    authors = db.relationship('Author', backref='publisher', lazy='dynamic')  # db.Column(db.String(80))  # ^^^
+    founders = db.Column(db.String(160))
+    books = db.relationship('Book', backref='publisher', lazy='dynamic')
+    authors = db.relationship('Author', backref='publisher', lazy='dynamic')
 
     def __init__(self, name, founding_date, headquarters, country, founders, books, authors):
+        """All string members are asserted to be len > 0"""
+
         self.name = name
-        assert (len(name) > 0)
+        assert len(name) > 0
 
         self.founding_date = founding_date
-        assert (len(founding_date) > 0)
+        assert len(founding_date) > 0
 
         self.headquarters = headquarters
-        assert (len(headquarters) > 0)
+        assert len(headquarters) > 0
 
         self.country = country
-        assert (len(country) > 0)
+        assert len(country) > 0
 
         self.founders = founders
-        assert (len(founders) > 0)
+        assert len(founders) > 0
 
         self.books = books
 
         self.authors = authors
 
 class Review(db.Model):
-    """Links to Book, Author"""
-    book = db.relationship('Book', uselist=False, backref='review', lazy='dynamic')
-    author = db.relationship('Author', uselist=False, backref='review', lazy='dynamic')
-    reviewer = db.Column(db.String(80))
+    """Links to Book, Author
+       Review-Book and Review-Author are one-to-one relationships"""
+    reviewer = db.Column(db.String(80), primary_key=True)
     rating = db.Column(db.Float)
     content = db.Column(db.String(80))
     source = db.Column(db.String(80))
+    book = db.relationship('Book', uselist=False, backref='review', lazy='dynamic')
+    author = db.relationship('Author', uselist=False, backref='review', lazy='dynamic')
 
     def __init__(self, book, author, reviewer, rating, content, source):
+        """All string members are asserted to be len > 0, rating is asserted to be > 0"""
         self.book = book
         self.author = author
 
         self.reviewer = reviewer
-        assert (len(reviewer) > 0)
+        assert len(reviewer) > 0
 
         self.rating = rating
-        assert (rating > 0)
+        assert rating > 0
 
         self.content = content
-        assert (len(content) > 0)
+        assert len(content) > 0
 
         self.source = source
-        assert (len(source) > 0)
+        assert len(source) > 0
