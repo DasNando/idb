5068348c475bb6cffac571904ea6e96c81339647
Fernando Mendoza <fernandom@utexas.edu>
2017-04-05 19:15:29 -0500

    adding app attribute to main

--- main.py
+++ main.py
@@ -2,49 +2,49 @@ from flask import render_template
 import logging
 from app import db
 
-local_app = db.app
+app = db.app
 
-@local_app.route('/')
-@local_app.route('/home')
+@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 
-@local_app.route('/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
 
-@local_app.route('/authors')
+@app.route('/authors')
 def authors():
     return render_template('authors.html')
 
 
-@local_app.route('/books')
+@app.route('/books')
 def books():
     return render_template('books.html')
 
 
-@local_app.route('/publishers')
+@app.route('/publishers')
 def publishers():
     return render_template('publishers.html')
 
-@local_app.route('/reviews')
+@app.route('/reviews')
 def reviews():
     return render_template('reviews.html')
 
 
-@local_app.route('/search')
+@app.route('/search')
 def search():
     return render_template('search.html')
 
 
-@local_app.errorhandler(500)
+@app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 
 if __name__ == '__main__':
-    local_app.run(debug=True)
+    app.run(debug=True)
 
