66535c8c77f7a9e6d1c37debf2338186e74f8cc0
Fernando Mendoza <fernandom@utexas.edu>
2017-04-07 02:20:02 -0500

    fixing assertion error

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, request
+from flask_restful import Api, Resource
 import logging
-from app import db
+from app import db, models
 
+db1 = db.db
 app = db.app
+
 
 @app.route('/')
 @app.route('/home')
@@ -29,6 +32,7 @@ def books():
 def publishers():
     return render_template('publishers.html')
 
+
 @app.route('/reviews')
 def reviews():
     return render_template('reviews.html')
@@ -39,12 +43,49 @@ def search():
     return render_template('search.html')
 
 
+# my token: wpV_1One91F2XNwmI6ukIg
+@app.route('/run_tests')
+def run_tests():
+    import requests
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Travis-API-Version': '3',
+        'Authorization': 'token wpV_1One91F2XNwmI6ukIg',
+    }
+
+    data = '{"request": {"branch": "master"}}'
+
+    requests.post('https://api.travis-ci.org/repo/DasNando%2Fidb/requests', headers=headers, data=data)
+
+    return render_template('search.html')
+
+
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 
+
+#################### API SECTION
+
+
+api = Api(app)
+book = {"book_test": "book_name"}
+
+
+class Q_Books(Resource):
+    def get(self, book_id):
+        return book
+
+    def put(self, book_id):
+        book = {book_id: book_id}
+        return book
+
+api.add_resource(Q_Books, '/<string:book_id>')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
