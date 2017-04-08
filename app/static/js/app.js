fd9639bf7cbacda15e1648deeeb483fcd9664cf7
Dylan Droll <dylan@cs.utexas.edu>
2017-04-07 21:49:50 -0500

    Please don't blow up part 2

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,10 +1,22 @@
-angular.module('sweReaders', ['angularUtils.directives.dirPagination'])
+var app = angular.module('sweReaders', ['angularUtils.directives.dirPagination']);
 
-.controller('BookController', function($scope){
+app.controller('BookController', function($scope/*, $http*/){
 
 	$scope.sortKey = 'title';
 	$scope.reverse = false;
 	$scope.searchBooks = '';
+
+// function Get(yourUrl){
+//   	 	var Httpreq = new XMLHttpRequest(); // a new request
+//   		Httpreq.open("GET",yourUrl,false);
+//   		Httpreq.send(null);
+//   		return Httpreq.responseText;          
+// }
+
+// $scope.booklist = JSON.parse(Get('https://www.googleapis.com/books/v1/volumes?q=subject:technology&maxResults=40&langRestrict=en&key=AIzaSyBIbqz2Qb-Afi-v1iXP3QRdM8wQmQMdFG4'))
+// console.log("got some json: "+$scope.booklist.kind)
+
+// $scope.booklist = $http.get("http://cs373-idb.appspot.com/api/books");
 
 	$scope.booklist = [
 		{
@@ -159,6 +171,222 @@ angular.module('sweReaders', ['angularUtils.directives.dirPagination'])
 			prices: "12"
 		}
 	];
+
+	$scope.sort = function(keyname){
+		if ($scope.sortKey != keyname) {
+			$scope.sortKey = keyname;
+			$scope.reverse = true;
+		};
+		$scope.reverse = !$scope.reverse;
+	}
+});
+
+app.controller('AuthorController', function($scope) {
+	$scope.sortKey = 'name';
+	$scope.reverse = false;
+	$scope.searchAuthors = '';
+
+	$scope.authorlist = [{
+		pic: "https://cs373-idb.appspot.com/static/img/authors/authors-static.jpg",
+		name: "Sir Writes-alot",
+		about: "Literally the greatest.",
+		dob: 1969,
+		dod: 2020,
+		works: "69 MILLION",
+	}, {
+		pic: "https://cs373-idb.appspot.com/static/img/authors/authors-static.jpg",
+		name: "Sir Writes-alot",
+		about: "Literally the greatest.",
+		dob: 1969,
+		dod: 2020,
+		works: "68 MILLION",
+	}, {
+		pic: "https://cs373-idb.appspot.com/static/img/authors/authors-static.jpg",
+		name: "Sir Writes-alot",
+		about: "Literally the greatest.",
+		dob: 1969,
+		dod: 2020,
+		works: "67 MILLION",
+	}, {
+		pic: "https://cs373-idb.appspot.com/static/img/authors/authors-static.jpg",
+		name: "Sir Writes-alot",
+		about: "Literally the greatest.",
+		dob: 1969,
+		dod: 2020,
+		works: "65 MILLION",
+	}, {
+		pic: "https://cs373-idb.appspot.com/static/img/authors/authors-static.jpg",
+		name: "Sir Writes-alot",
+		about: "Literally the greatest.",
+		dob: 1969,
+		dod: 2020,
+		works: "66 MILLION",
+	}, {
+		pic: "https://cs373-idb.appspot.com/static/img/authors/authors-static.jpg",
+		name: "Sir Writes-alot",
+		about: "Literally the greatest.",
+		dob: 1969,
+		dod: 2020,
+		works: "70 MILLION",
+	}];
+
+	$scope.sort = function(keyname){
+		if ($scope.sortKey != keyname) {
+			$scope.sortKey = keyname;
+			$scope.reverse = true;
+		};
+		$scope.reverse = !$scope.reverse;
+	}
+});
+
+app.controller('PublisherController', function($scope) {
+	$scope.sortKey = 'name';
+	$scope.reverse = false;
+	$scope.searchPublishers = '';
+
+	$scope.publisherlist = [{
+		pic: "1",
+		name: "1",
+		about: "1",
+		headquarters: "1",
+		country: "1",
+		founded: "1"
+	}, {
+		pic: "2",
+		name: "2",
+		about: "2",
+		headquarters: "2",
+		country: "2",
+		founded: "2"
+	}, {
+		pic: "3",
+		name: "3",
+		about: "3",
+		headquarters: "3",
+		country: "3",
+		founded: "3"
+	}, {
+		pic: "1",
+		name: "1",
+		about: "1",
+		headquarters: "1",
+		country: "1",
+		founded: "1"
+	}, {
+		pic: "2",
+		name: "2",
+		about: "2",
+		headquarters: "2",
+		country: "2",
+		founded: "2"
+	}, {
+		pic: "3",
+		name: "3",
+		about: "3",
+		headquarters: "3",
+		country: "3",
+		founded: "3"
+	}, {
+		pic: "1",
+		name: "1",
+		about: "1",
+		headquarters: "1",
+		country: "1",
+		founded: "1"
+	}, {
+		pic: "2",
+		name: "2",
+		about: "2",
+		headquarters: "2",
+		country: "2",
+		founded: "2"
+	}, {
+		pic: "3",
+		name: "3",
+		about: "3",
+		headquarters: "3",
+		country: "3",
+		founded: "3"
+	}];
+
+	$scope.sort = function(keyname){
+		if ($scope.sortKey != keyname) {
+			$scope.sortKey = keyname;
+			$scope.reverse = true;
+		};
+		$scope.reverse = !$scope.reverse;
+	}
+});
+
+app.controller('ReviewController', function($scope) {
+	$scope.sortKey = 'name';
+	$scope.reverse = false;
+	$scope.searchReviews = '';
+
+	$scope.reviewlist = [{
+		pic: "1",
+		reviewer: "1",
+		rating: "1",
+		content: "1",
+		source: "1",
+		book: "1"
+	}, {
+		pic: "2",
+		reviewer: "2",
+		rating: "2",
+		content: "2",
+		source: "2",
+		book: "2"
+	}, {
+		pic: "3",
+		reviewer: "3",
+		rating: "3",
+		content: "3",
+		source: "3",
+		book: "3"
+	}, {
+		pic: "1",
+		reviewer: "1",
+		rating: "1",
+		content: "1",
+		source: "1",
+		book: "1"
+	}, {
+		pic: "2",
+		reviewer: "2",
+		rating: "2",
+		content: "2",
+		source: "2",
+		book: "2"
+	}, {
+		pic: "3",
+		reviewer: "3",
+		rating: "3",
+		content: "3",
+		source: "3",
+		book: "3"
+	}, {
+		pic: "1",
+		reviewer: "1",
+		rating: "1",
+		content: "1",
+		source: "1",
+		book: "1"
+	}, {
+		pic: "2",
+		reviewer: "2",
+		rating: "2",
+		content: "2",
+		source: "2",
+		book: "2"
+	}, {
+		pic: "3",
+		reviewer: "3",
+		rating: "3",
+		content: "3",
+		source: "3",
+		book: "3"
+	}];
 
 	$scope.sort = function(keyname){
 		if ($scope.sortKey != keyname) {
