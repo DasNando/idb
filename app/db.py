70b03d203c6bde7fbb51b3a392d8a5e6fc648291
Fernando Mendoza <fernandom@utexas.edu>
2017-04-07 20:34:46 -0500

    updating config

--- app/db.py
+++ app/db.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 app = Flask(__name__)
 # dialect+driver://username:password@host:port/database
 # postgresql://scott:tiger@localhost/mydatabase
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'postgresql://postgres:swereaders@35.184.110.238:5432/postgres'
+app.config.from_json(os.getcwd() + "/config.json")
 db = SQLAlchemy(app)
