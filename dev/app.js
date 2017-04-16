e4365ce4aa73c7f8b8a56dae702c16cdab918a67
Dylan Droll <dylan@cs.utexas.edu>
2017-04-16 03:04:50 -0500

    Phase 2 mostly completed kek

--- dev/app.js
+++ dev/app.js
@@ -6,173 +6,19 @@ app.controller('BookController', function($scope, $http){
 	$scope.reverse = false;
 	$scope.searchBooks = '';
 
-	var myUrl = 'http://cs373-idb.appspot.com/api/books/params&genre=Science';
+	var myUrl = 'https://cs373-idb.appspot.com/api/books/params&genre=Fiction';
 
 	$scope.booklist = [];
 
 	$http.get(myUrl).success(function(data) {
-		var myjson = JSON.parse(data);
+		var myjson = data/*JSON.parse(data)*/;
 		$scope.booklist = myjson;
 	});
 
-
-	// $scope.booklist = $http.get("http://cs373-idb.appspot.com/api/books/params&genre=Science");
-
-	// $scope.booklist = [
-	// 	{
-	// 		coverart: "",
-	// 		title: "Ender's Game",
-	// 		author: "Orson Scott Card",
-	// 		published: "January 15, 1985",
-	// 		rating: 4.8,
-	// 		publisher: "Publish McPublishson",
-	// 		genre: "Good Book",
-	// 		isbn: "MAMBO #6",
-	// 		prices: "$7?"
-	// 	}, {
-	// 		coverart: "",
-	// 		title: "Slaughterhouse-Five",
-	// 		author: "Kurt Vonnegut",
-	// 		published: "March 1969",
-	// 		rating: 3.9,
-	// 		publisher: "Publish McPublishson",
-	// 		genre: "Good Book",
-	// 		isbn: "MAMBO #3",
-	// 		prices: "$9?"
-	// 	}, {
-	// 		coverart: "",
-	// 		title: "The Great Gatsby",
-	// 		author: "F Scott Fitzgerald",
-	// 		published: "1925",
-	// 		rating: 5.0,
-	// 		publisher: "Publish McPublishson",
-	// 		genre: "Good Book",
-	// 		isbn: "MAMBO #5",
-	// 		prices: "$10?"
-	// 	}, {
-	// 		coverart: "1",
-	// 		title: "1",
-	// 		author: "1",
-	// 		published: "1",
-	// 		rating: "1",
-	// 		publisher: "1",
-	// 		genre: "1",
-	// 		isbn: "1",
-	// 		prices: "1"
-	// 	}, {
-	// 		coverart: "2",
-	// 		title: "2",
-	// 		author: "2",
-	// 		published: "2",
-	// 		rating: "2",
-	// 		publisher: "2",
-	// 		genre: "2",
-	// 		isbn: "2",
-	// 		prices: "2"
-	// 	}, {
-	// 		coverart: "3",
-	// 		title: "3",
-	// 		author: "3",
-	// 		published: "3",
-	// 		rating: "3",
-	// 		publisher: "3",
-	// 		genre: "3",
-	// 		isbn: "3",
-	// 		prices: "3"
-	// 	}, {
-	// 		coverart: "4",
-	// 		title: "4",
-	// 		author: "4",
-	// 		published: "4",
-	// 		rating: "4",
-	// 		publisher: "4",
-	// 		genre: "4",
-	// 		isbn: "4",
-	// 		prices: "4"
-	// 	}, {
-	// 		coverart: "5",
-	// 		title: "5",
-	// 		author: "5",
-	// 		published: "5",
-	// 		rating: "5",
-	// 		publisher: "5",
-	// 		genre: "5",
-	// 		isbn: "5",
-	// 		prices: "5"
-	// 	}, {
-	// 		coverart: "6",
-	// 		title: "6",
-	// 		author: "6",
-	// 		published: "6",
-	// 		rating: "6",
-	// 		publisher: "6",
-	// 		genre: "6",
-	// 		isbn: "6",
-	// 		prices: "6"
-	// 	}, {
-	// 		coverart: "7",
-	// 		title: "7",
-	// 		author: "7",
-	// 		published: "7",
-	// 		rating: "7",
-	// 		publisher: "7",
-	// 		genre: "7",
-	// 		isbn: "7",
-	// 		prices: "7"
-	// 	}, {
-	// 		coverart: "8",
-	// 		title: "8",
-	// 		author: "8",
-	// 		published: "8",
-	// 		rating: "8",
-	// 		publisher: "8",
-	// 		genre: "8",
-	// 		isbn: "8",
-	// 		prices: "8"
-	// 	}, {
-	// 		coverart: "9",
-	// 		title: "9",
-	// 		author: "9",
-	// 		published: "9",
-	// 		rating: "9",
-	// 		publisher: "9",
-	// 		genre: "9",
-	// 		isbn: "9",
-	// 		prices: "9"
-	// 	}, {
-	// 		coverart: "10",
-	// 		title: "10",
-	// 		author: "10",
-	// 		published: "10",
-	// 		rating: "10",
-	// 		publisher: "10",
-	// 		genre: "10",
-	// 		isbn: "10",
-	// 		prices: "10"
-	// 	}, {
-	// 		coverart: "11",
-	// 		title: "11",
-	// 		author: "11",
-	// 		published: "11",
-	// 		rating: "11",
-	// 		publisher: "11",
-	// 		genre: "11",
-	// 		isbn: "11",
-	// 		prices: "11"
-	// 	}, {
-	// 		coverart: "12",
-	// 		title: "12",
-	// 		author: "12",
-	// 		published: "12",
-	// 		rating: "12",
-	// 		publisher: "12",
-	// 		genre: "12",
-	// 		isbn: "12",
-	// 		prices: "12"
-	// 	}
-	// ];
-
-	console.log("NIGGA PLEASE WORK: "+$scope.booklist.length);
+	$scope.searchFilter = function (obj) {
+        var re = new RegExp($scope.searchBooks, 'i');
+        return !$scope.searchBooks || re.test(obj.title) || re.test(obj.author) || re.test(obj.year) || re.test(obj.publisher) || re.test(obj.rating) || re.test(obj.genre);
+    };
 
 	$scope.sort = function(keyname){
 		if ($scope.sortKey != keyname) {
