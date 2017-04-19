803f0e3c4cec32749231c97dbaf2d34811351c9c
Conner Newman <conner@cs.utexas.edu>
2017-04-19 16:46:06 -0500

    more authors

--- scraper/auth_names.py
+++ scraper/auth_names.py
@@ -6,13 +6,16 @@ def path(f):
 with open(path("book_test3.json")) as books3_json:
 	books3_data = json.load(books3_json)
 
+with open(path('books.json')) as books_json:
+	books_data = json.load(books_json)
+
 auths = set()
 
-for search in books3_data['books']:
+for search in (books_data["books"] + books3_data['books']):
 	for item in search['items']:
 		try:
-			name = item['volumeInfo']["authors"][0]
-			auths.add(name)
+			for name in item['volumeInfo']["authors"]:
+				auths.add(name)
 		except:
 			continue
 
