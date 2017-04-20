d789fd4f7c356db594cabf57649c405d8146d023
Fernando Mendoza <fernandom@utexas.edu>
2017-04-20 01:13:46 -0500

    okay, this should work

--- app/db.py
+++ app/db.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 # dialect+driver://username:password@host:port/database
 # postgresql://scott:tiger@localhost/mydatabase
 # app.config.from_json(os.getcwd() + "/config.json")
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:swe-readers@/postgres?host=/cloudsql/cs373-idb:us-central1:idb"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:swe-readers@/postgres?host=/cloudsql/cs373-idb:us-central1:idb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
